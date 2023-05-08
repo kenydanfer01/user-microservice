4577d8bf4f8dd3469b4956bc62328a23dc202592
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-08 18:58:36 -0500

    Add OwnerRestController in driving/http

---
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/OwnerRestController.java
@@ -0,0 +1,48 @@
+package com.pragma.powerup.usermicroservice.adapters.driving.http.controller;
+
+import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request.UserRequestDto;
+import com.pragma.powerup.usermicroservice.adapters.driving.http.handlers.IUserHandler;
+import com.pragma.powerup.usermicroservice.configuration.Constants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static com.pragma.powerup.usermicroservice.configuration.Constants.OWNER_ROLE_ID;
+
+@RestController
+@RequestMapping("/owner")
+@RequiredArgsConstructor
+@SecurityRequirement(name = "jwt")
+public class OwnerRestController {
+
+    private final IUserHandler userHandler;
+
+    @Operation(summary = "Add a new owner",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Owner created",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Map"))),
+                    @ApiResponse(responseCode = "409", description = "User already exists",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error"))),
+                    @ApiResponse(responseCode = "403", description = "Role not allowed for user creation",
+                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
+    @PostMapping("")
+    public ResponseEntity<Map<String, String>> saveOwner(@Valid @RequestBody UserRequestDto userRequestDto) {
+        userRequestDto.setIdRole(OWNER_ROLE_ID);
+        userHandler.saveUser(userRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.USER_CREATED_MESSAGE));
+    }
+}
