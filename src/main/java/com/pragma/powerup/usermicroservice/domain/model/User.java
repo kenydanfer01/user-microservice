7780e2adcbe3ed5d0c24116604afeff3db6ca672
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-08 18:40:09 -0500

    Update User model to include more attributes

--- src/main/java/com/pragma/powerup/usermicroservice/domain/model/User.java
+++ src/main/java/com/pragma/powerup/usermicroservice/domain/model/User.java
@@ -1,13 +1,27 @@
 package com.pragma.powerup.usermicroservice.domain.model;
 
+import java.time.LocalDate;
+
 public class User {
     private Long id;
-    private Person person;
+    private String name;
+    private String surname;
+    private String dni;
+    private String phone;
+    private LocalDate birthday;
+    private String mail;
+    private String password;
     private Role role;
 
-    public User(Long id, Person person, Role role) {
+    public User(Long id, String name, String surname, String dni, String phone, LocalDate birthday, String mail, String password, Role role) {
         this.id = id;
-        this.person = person;
+        this.name = name;
+        this.surname = surname;
+        this.dni = dni;
+        this.phone = phone;
+        this.birthday = birthday;
+        this.mail = mail;
+        this.password = password;
         this.role = role;
     }
 
@@ -19,12 +33,60 @@ public class User {
         this.id = id;
     }
 
-    public Person getPerson() {
-        return person;
+    public String getName() {
+        return name;
     }
 
-    public void setPerson(Person person) {
-        this.person = person;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Role getRole() {
