5d4d46097e74d3a793fecab9af4f01beca1a1b08
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-08 18:52:55 -0500

    Update UserDetailsServiceImpl in driven/jpa/mysql

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/adapter/UserDetailsServiceImpl.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/adapter/UserDetailsServiceImpl.java
@@ -1,8 +1,6 @@
 package com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.adapter;
 
-import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories.IPersonRepository;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories.IUserRepository;
-import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.PersonEntity;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.PrincipalUser;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.RoleEntity;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.UserEntity;
@@ -12,30 +10,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    IPersonRepository personRepository;
-    @Autowired
     IUserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String documentID) throws UsernameNotFoundException {
-        PersonEntity usuario = personRepository.findByDniNumber(documentID).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
-        List<UserEntity> userEntity = userRepository.findAllByPersonEntityId(usuario.getId());
-        if (userEntity.isEmpty()) {
-            throw new UsernameNotFoundException("User not found with documentID: " + documentID);
-        }
-        List<RoleEntity> roles = new ArrayList<>();
+    public UserDetails loadUserByUsername(String dni) throws UsernameNotFoundException {
 
-        for (UserEntity user : userEntity) {
-            roles.add(user.getRoleEntity());
-        }
+        UserEntity user = userRepository.findByDni(dni).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
 
-        return PrincipalUser.build(usuario, roles);
+        RoleEntity role = user.getRoleEntity();
+
+        return PrincipalUser.build(user, role);
     }
 }
