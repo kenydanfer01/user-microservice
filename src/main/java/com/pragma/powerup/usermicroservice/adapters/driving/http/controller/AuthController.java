0fe295d8b4a6ddb51bdc32988377e2b8210c8dfa
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-08 18:58:08 -0500

    Update AuthController and UserRestController in driving/http

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/AuthController.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/UserRestController.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/UserRestController.java
@@ -1,12 +1,10 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.controller;
 
 import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request.UserRequestDto;
-import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.response.PersonResponseDto;
+import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.response.UserResponseDto;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.handlers.IUserHandler;
 import com.pragma.powerup.usermicroservice.configuration.Constants;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -20,11 +18,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -48,6 +44,8 @@ public class UserRestController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.USER_CREATED_MESSAGE));
     }
+
+
     @Operation(summary = "Delete an user",
             responses = {
                     @ApiResponse(responseCode = "200", description = "User deleted",
@@ -59,45 +57,28 @@ public class UserRestController {
         userHandler.deleteUser(userRequestDto);
         return ResponseEntity.ok(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.USER_DELETED_MESSAGE));
     }
-    @Operation(summary = "Get all the providers",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "All providers returned",
-                            content = @Content(mediaType = "application/json",
-                                    array = @ArraySchema(schema = @Schema(implementation = PersonResponseDto.class)))),
-                    @ApiResponse(responseCode = "404", description = "No data found",
-                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
-    @GetMapping("/provider")
-    public ResponseEntity<List<PersonResponseDto>> getAllProviders(@Parameter(description = "Number of the page to list providers") @RequestParam int page) {
-        return ResponseEntity.ok(userHandler.getProvider(page));
-    }
-    @Operation(summary = "Get a provider user",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Provider user returned",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonResponseDto.class))),
-                    @ApiResponse(responseCode = "404", description = "User not found with provider role",
-                            content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
-    @GetMapping("/provider/{id}")
-    public ResponseEntity<PersonResponseDto> getProvider(@PathVariable Long id) {
-        return ResponseEntity.ok(userHandler.getProvider(id));
-    }
+
+
     @Operation(summary = "Get a employee user",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Employee user returned",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonResponseDto.class))),
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
                     @ApiResponse(responseCode = "404", description = "User not found with employee role",
                             content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
     @GetMapping("/employee/{id}")
-    public ResponseEntity<PersonResponseDto> getEmployee(@PathVariable Long id) {
+    public ResponseEntity<UserResponseDto> getEmployee(@PathVariable Long id) {
         return ResponseEntity.ok(userHandler.getEmployee(id));
     }
+
+
     @Operation(summary = "Get a client user",
             responses = {
                     @ApiResponse(responseCode = "200", description = "Client user returned",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonResponseDto.class))),
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
                     @ApiResponse(responseCode = "404", description = "User not found with client role",
                             content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
     @GetMapping("/client/{id}")
-    public ResponseEntity<PersonResponseDto> getClient(@PathVariable Long id) {
+    public ResponseEntity<UserResponseDto> getClient(@PathVariable Long id) {
         return ResponseEntity.ok(userHandler.getClient(id));
     }
 }
