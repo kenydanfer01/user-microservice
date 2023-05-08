5a255f2f537e09ce4f8cab94bc281cd38e1526b0
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-08 18:46:07 -0500

    Update UserEntity in driven/jpa/mysql

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/entity/UserEntity.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/entity/UserEntity.java
@@ -1,16 +1,12 @@
 package com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "user")
@@ -22,9 +18,29 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "id_person")
-    private PersonEntity personEntity;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(unique = true, nullable = false, length = 10)
+    private String dni;
+
+    @Column(unique = true, nullable = false, length = 13)
+    private String phone;
+
+    private LocalDate birthday;
+
+    @Column(nullable = false)
+    private String mail;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String tokenPassword;
+
     @ManyToOne
     @JoinColumn(name = "id_role")
     private RoleEntity roleEntity;
