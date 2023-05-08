24cf580e2500cdb44d55fa547bf628fd34c98ddf
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-08 18:55:34 -0500

    Update UserRequestDto and UserResponseDto in driving/http

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/UserRequestDto.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/UserRequestDto.java
@@ -1,11 +1,47 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @Getter
 public class UserRequestDto {
-    private Long idPerson;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String surname;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{1,10}", message = "DNI must be numeric and have a maximum length of 10 characters")
+    private String dni;
+
+    @NotBlank
+    @Pattern(regexp = "^\\+?\\d{1,12}$", message = "Phone number must be a sequence of up to 13 digits, optionally starting with a '+' sign")
+    private String phone;
+
+    @Past(message = "Date of birth must be in the past")
+    private LocalDate birthday;
+
+    @NotBlank
+    @Email(message = "Invalid email format")
+    private String mail;
+
+    @NotBlank
+    private String password;
+
+    @JsonIgnore
     private Long idRole;
+
+    public void setIdRole(Long idRole) {
+        this.idRole = idRole;
+    }
 }
--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/response/UserResponseDto.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/response/UserResponseDto.java
@@ -3,9 +3,17 @@ package com.pragma.powerup.usermicroservice.adapters.driving.http.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @Getter
 public class UserResponseDto {
-    private Long idPerson;
+    private String name;
+    private String surname;
+    private String dni;
+    private String phone;
+    private LocalDate birthday;
+    private String mail;
+    private String password;
     private Long idRole;
 }
