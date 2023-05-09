cd4f3c597e3fee0daa89b032988e78d8de0ace1b
kenydanfer01 <kenychumacero19@gmail.com>
2023-05-08 19:05:00 -0500

    Update data.sql to start project

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,33 +1,33 @@
-INSERT INTO `person` (
+INSERT INTO `role` (`id`, `description`, `name`) VALUES ('1', 'ROLE_ADMIN', 'ROLE_ADMIN');
+INSERT INTO `role` (`id`, `description`, `name`) VALUES ('2', 'ROLE_OWNER', 'ROLE_OWNER');
+INSERT INTO `role` (`id`, `description`, `name`) VALUES ('1', 'ROLE_EMPLOYEE', 'ROLE_EMPLOYEE');
+INSERT INTO `role` (`id`, `description`, `name`) VALUES ('2', 'ROLE_CLIENT', 'ROLE_CLIENT');
+
+
+INSERT INTO `user` (
     `id`,
-    `address`,
-    `dni_number`,
-    `id_dni_type`,
-    `id_person_type`,
-    `mail`,
     `name`,
-    `password`,
-    `phone`,
     `surname`,
-    `token_password`
+    `dni`,
+    `phone`,
+    `birthday`,
+    `mail`,
+    `password`,
+    `token_password`,
+    `id_role`
   )
 VALUES
   (
     '1',
-    'st 123 # 456',
-    '123',
-    '1',
-    '1',
-    'email@some.com',
-    'Name',
+    'Keny',
+    'Chumacero',
+    '74',
+    '+51949469604',
+    '2000-01-19',
+    'keny@gmail.com',
     '$2a$10$GlsGSNhkbVon6ZOSNMptOu5RikedRzlCAhMa7YpwvUSS0c88WT99S',
-    '1234567890',
-    'Surname',
-    NULL
+    NULL,
+    '1'
   );
 
 
-INSERT INTO `role` (`id`, `description`, `name`) VALUES ('1', 'ROLE_ADMIN', 'ROLE_ADMIN');
-INSERT INTO `role` (`id`, `description`, `name`) VALUES ('2', 'ROLE_USER', 'ROLE_USER');
-
-INSERT INTO `user` (`id_person`, `id_role`) VALUES ('1', '1');
